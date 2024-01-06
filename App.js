ab04d23634b144eb606ef6e4037f6fef90a1f0fd
Orel hale <orelh.ravtech.co.il>
2024-01-06 19:24:33 +0200

    init files

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Layout from './app/layout/Layout';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My Shas App</Text>
       <Text>Orel hale</Text>
+      <Layout />
       <StatusBar style="auto" />
     </View>
   );
